cd152dab5e737fc4a43f39af96b73d924506e3c3
prazUkesh <esh3ac@gmail.com>
2025-05-04 08:49:47 +0545

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,42 +1,55 @@
 class Customer{
-    double  balance = 5000;
-    synchronized  void withdraw (double amount){
-        if(amount> balance){
-            try {
-                wait();
-            } catch(Exception e) {
-            System.out.println("Exception caught: "+e);
-            }
-        }
-        balance-=amount;
-        System.out.println("withdrawn: "+amount);
-        System.out.println("balance "+balance);
-        
+  double balance = 5000;
+  synchronized void withdraw(double amount){
+    if(amount>balance){
+      try {
+        System.out.println("Insufficient balance, waiting for deposit...");
+        wait();
+      } catch (Exception e) {
+        System.out.println("Exception caught: "+e);
+
+      }
     }
-}
+    balance -= amount;
+    System.out.println("Withdrawn: "+amount);
+    System.out.println("Balance: "+balance);
+    
+  }
 
-
-synchronized void deposit(double amount){
+  synchronized void deposit(double amount){
     System.out.println("Depositing: "+amount);
     balance += amount;
-    System.out.println("deposited "+amount);
-    System.out.println("balance "+balance);
+    System.out.println("Deposited: "+amount);
+    System.out.println("Balance: "+balance);
+    notify();
+  }
 }
 
-public class Main
-{
-	public static void main(String[] args) {
-		Customer c  = new Customer();
-		Thread t1= new Thread(new Runnable(){
-		    public void run(){
-		         c.withdraw(1000);
-		    }
-		});
-		
-		Thread t2 = new Thread(new Runnable(){
-		    public void run(){
-		        c.deposit(15000);
-		    }
-		});
-	}
+
+
+
+
+
+public class main{
+  public static void main(String[] args) {
+    
+    Customer c = new Customer();
+    Thread t1 = new Thread(new Runnable() {
+      public void run() {
+        c.withdraw(10000);
+      }
+      
+    } );
+    Thread t2 = new Thread(new Runnable() {
+      public void run() {
+        c.deposit(6000);
+      }
+      
+    } );
+    t1.start();
+    t2.start();
+    
+    
+    
+  }
 }
